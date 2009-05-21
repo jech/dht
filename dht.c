e23cae7eedcfca5796ee61e4e8398fb01eacf20f
Juliusz Chroboczek <jch@pps.jussieu.fr>
2009-05-21 22:03:18 +0200

    Implement xorcmp.

--- dht.c
+++ dht.c
@@ -319,6 +319,26 @@ common_bits(const unsigned char *id1, const unsigned char *id2)
     return 8 * i + j;
 }
 
+/* Determine whether id1 or id2 is closer to ref */
+int
+xorcmp(const unsigned char *id1, const unsigned char *id2,
+       const unsigned char *ref)
+{
+    int i;
+    for(i = 0; i < 20; i++) {
+        unsigned char xor1, xor2;
+        if(id1[i] == id2[i])
+            continue;
+        xor1 = id1[i] ^ ref[i];
+        xor2 = id2[i] ^ ref[i];
+        if(xor1 < xor2)
+            return -1;
+        else
+            return 1;
+    }
+    return 0;
+}
+
 /* We keep buckets in a sorted linked list.  A bucket b ranges from
    b->first inclusive up to b->next->first exclusive. */
 static int
