bd084f0037a42b1b617a4fe88833a7fb86466203
Juliusz Chroboczek <jch@pps.jussieu.fr>
2009-09-05 22:45:12 +0200

    Fix filling of search bucket.

--- dht.c
+++ dht.c
@@ -1036,7 +1036,7 @@ dht_search(int s, const unsigned char *id, int port,
     b = find_bucket(id);
     insert_search_bucket(b, sr);
 
-    if(sr->numnodes < 8) {
+    if(sr->numnodes < SEARCH_NODES) {
         struct bucket *p = previous_bucket(b);
         if(b->next)
             insert_search_bucket(b->next, sr);
